93091da04baf2fa28ed27ccc4a4eeec42e0aa598
moot attar <mootattar@gmail.com>
2025-01-06 23:09:40 +0300

    fix some gubs when filters

--- src/shared/filteredTodos.js
+++ src/shared/filteredTodos.js
@@ -2,31 +2,38 @@ export const filterTodos = (todos, selected) => {
   switch (selected) {
     case "Home":
       return todos.filter(
-        (todo) => todo.cat === "Home" && todo.endOfDay > todo.date
+        (todo) =>
+          todo.cat === "Home" && todo.endOfDay > new Date().toISOString()
       );
     case "Work":
       return todos.filter(
-        (todo) => todo.cat === "Work" && todo.endOfDay > todo.date
+        (todo) =>
+          todo.cat === "Work" && todo.endOfDay > new Date().toISOString()
       );
     case "Personal":
       return todos.filter(
-        (todo) => todo.cat === "Personal" && todo.endOfDay > todo.date
+        (todo) =>
+          todo.cat === "Personal" && todo.endOfDay > new Date().toISOString()
       );
     case "Health":
       return todos.filter(
-        (todo) => todo.cat === "Health" && todo.endOfDay > todo.date
+        (todo) =>
+          todo.cat === "Health" && todo.endOfDay > new Date().toISOString()
       );
     case "Study":
       return todos.filter(
-        (todo) => todo.cat === "Study" && todo.endOfDay > todo.date
+        (todo) =>
+          todo.cat === "Study" && todo.endOfDay > new Date().toISOString()
       );
     case "Devotional":
       return todos.filter(
-        (todo) => todo.cat === "Devotional" && todo.endOfDay > todo.date
+        (todo) =>
+          todo.cat === "Devotional" && todo.endOfDay > new Date().toISOString()
       );
     case "Other":
       return todos.filter(
-        (todo) => todo.cat === "Other" && todo.endOfDay > todo.date
+        (todo) =>
+          todo.cat === "Other" && todo.endOfDay > new Date().toISOString()
       );
     case "Completed":
       return todos.filter((todo) => todo.ischecked);
